cff17e8b65ed4991863ffae22acc2bc7efbde0be
BasedOnTechnology <therobotist@gmail.com>
2016-08-26 20:59:58 -0400

    More betterer.

--- PyLuminescence/PyLuminescence.py
+++ PyLuminescence/PyLuminescence.py
@@ -8,9 +8,15 @@ import struct
 from array import array
 import pygame
 import Tkinter
+from Tkinter import *
+import json
 from pygame.locals import *
 from pygame.mixer import Sound, get_init, pre_init
 
+#config = open("PyLuminescence.config",'r+')
+global best_frequency
+global frequency_range
+frequency_range = [21000,25000]
 class Note(Sound):
 
     def __init__(self, frequency, volume=.1):
@@ -28,9 +34,8 @@ class Note(Sound):
             else:
                 samples[time] = -amplitude
         return samples
-frequency = 23000
 p = PyAudio.PyAudio()
-stream = p.open(format=p.get_format_from_width(1), # 8bit
+stream = p.open(format=PyAudio.paInt16, # 8bit
                     channels=1, # mono
                     rate=96000,
                     input=True,
@@ -38,14 +43,43 @@ stream = p.open(format=p.get_format_from_width(1), # 8bit
                     frames_per_buffer=2048)
 pre_init(96000, -16, 1, 1024)
 pygame.init()
-best_frequency = 0
-amplitude = 0
 
 top = Tkinter.Tk()
 top.title("PyLuminescence")
 
-def helloCallBack():
-   tkMessageBox.showinfo( "Hello Python", "Hello World")
-B = Tkinter.Button(top, text ="Hello", command = helloCallBack)
-B.pack()
+
+def Tune():
+    volume = 0.4
+    temp_best_amplitude=0.0
+    temp_best_frequency=0.0
+    for frequency in range(21000,25000,10):
+        Note(frequency,volume).play(-1)
+        sleep(0.1)
+        stream.start_stream()
+        data=stream.read(2048)
+        stream.stop_stream()
+        amplitude = audioop.maxpp(data,2)
+        Note(frequency).stop()
+        w.create_rectangle(int(((frequency-frequency_range[0])/float(frequency_range[1]-frequency_range[0]))*500.0), 50, int(((frequency-frequency_range[0])/float(frequency_range[1]-frequency_range[0]))*500.0)+1, 50-((amplitude/65536.0)*50), fill="blue")
+        w.update()
+        #w.delete("all")
+        if(amplitude > temp_best_amplitude):
+            temp_best_amplitude=amplitude
+            temp_best_frequency=frequency
+            display_amplitude.configure(text=("Best Amplitude: "+ str(amplitude)))
+            display_amplitude.configure(text=("Best Frequency: "+ str(frequency)))
+B = Tkinter.Button(top, text ="Tune", command = Tune)
+B.grid(row=0,column=1)
+display_amplitude = Label(top, text="") # we need this Label as a variable!
+display_amplitude.grid(row=1,column=1)
+display_amplitude.configure(text="Amplitude: 0")
+
+
+display_frequency = Label(top, text="") # we need this Label as a variable!
+display_frequency.grid(row=2,column=1)
+display_frequency.configure(text="Frequency: 0")
+
+w = Canvas(top, width=500, height=50)
+w.grid(row=3,column=1)
+
 top.mainloop()
